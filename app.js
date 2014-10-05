9386f912dcffdcfc8dcae4b5b623e8380538449f
Martin Jezek <info@martin-jezek.com>
2014-10-05 14:19:06 +0100

    feat: unify app structure #1

--- app.js
+++ app.js
@@ -6,10 +6,10 @@ var config      = require('config'),
     express     = require('express'),
     path        = require('path');
 
-var router = {
-    index       : require('./router/index.js'),
-    user        : require('./router/user.js'),
-    group       : require('./router/group.js'),
+var routes = {
+    index       : require('./routes/index.js'),
+    user        : require('./routes/user.js'),
+    group       : require('./routes/group.js'),
 };
 
 var app = express();
@@ -18,9 +18,9 @@ var app = express();
 app.use(express.static(path.join(__dirname, '/public')));
 
 // app routes
-app.use('/', router.index.router);
-app.use('/user', router.user.router);
-app.use('/group', router.group.router);
+app.use('/', routes.index.router);
+app.use('/user', routes.user.router);
+app.use('/group', routes.group.router);
 
 http.createServer(app).listen(config.get('http.port', config.get('http.host')), function() {
     console.log('[config:' + config.get('name') + '] -> Server running at http://' + config.get('http.host') + ':' + config.get('http.port') + '/');
