da5bb1761fe61f4d296c23cb69de2dd7c5305c0f
Martin Jezek <info@martin-jezek.com>
2014-10-08 23:05:13 +0100

    feat: add all CRUD end-points for Groups #1

--- routes/group.js
+++ routes/group.js
@@ -1,13 +1,14 @@
 'use strict';
 
-var express = require('express');
+var express     = require('express'),
+    bodyParser  = require('body-parser'),
+    group       = require('../lib/group');
+
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
 
 module.exports = express.Router()
-
-    .get('/', function(req, res, next) {
-        res.send('List of Groups');
-    })
-
-    .get('/:id', function(req, res, next) {
-        res.send('Group ' + req.params.id);
-    });
+    .get(   '/'                         , group.list)
+    .post(  '/'     , urlencodedParser  , group.create)
+    .get(   '/:id'                      , group.read)
+    .put(   '/:id'  , urlencodedParser  , group.update)
+    .delete('/:id'                      , group.delete);
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,6 @@
 var express = require('express');
 
 module.exports = express.Router()
-
     .get('/', function(req, res) {
         res.send('API is ready to use.');
     });
