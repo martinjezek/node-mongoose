0f855c469e43e2fdf37c9c7222fbf666f0a09f93
Martin Jezek <info@martin-jezek.com>
2014-12-07 18:49:30 +0000

    fix: cross-domain issues such as POST, DELETE #12

--- routes/user.js
+++ routes/user.js
@@ -1,17 +1,14 @@
 'use strict';
 
 var express     = require('express'),
-    bodyParser  = require('body-parser'),
     user        = require('../lib/model/user');
-
-var urlencodedParser = bodyParser.urlencoded({ extended: false });
 
 module.exports = express.Router()
     // middleware
     .use(                                     user.middleware)
     // user
     .get('/'                                , user.list)
-    .post('/'           , urlencodedParser  , user.create)
+    .post('/'                               , user.create)
     .get('/:id'                             , user.read)
-    .put('/:id'         , urlencodedParser  , user.update)
+    .put('/:id'                             , user.update)
     .delete('/:id'                          , user.delete);
